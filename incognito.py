1a621e7db6a1bcb292d1d45b4150996099c4b517
Divadsjo <70754118+Divadsjo@users.noreply.github.com>
2020-11-13 13:07:23 +0100

    Update incognito.py

--- incognito.py
+++ incognito.py
@@ -13,4 +13,4 @@ for x in range(tries):
             eyewear += 1
         elif plag[1] == "face":
             face += 1
-    print(face*eyewear*headgear)
+    print(face*eyewear*headgear+face+eyewear+headgear)
